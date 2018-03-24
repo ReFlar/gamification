96a815b50c744bcc477c2fcb9ee87b6ca453d61e
Charlie <charlie.kern01@gmail.com>
2018-03-24 06:46:19 +0000

    Apply fixes from StyleCI

--- src/Listeners/SaveVotesToDatabase.php
+++ src/Listeners/SaveVotesToDatabase.php
@@ -159,9 +159,9 @@ class SaveVotesToDatabase
     public function sendData($post, $user, $actor, $type, $before)
     {
         $oldVote = Notification::where([
-            'sender_id' => $actor->id,
+            'sender_id'  => $actor->id,
             'subject_id' => $post->id,
-            'data' => '"'.$before.'"',
+            'data'       => '"'.$before.'"',
         ])->first();
 
         if ($oldVote) {
